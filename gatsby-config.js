824117e14e6c550f189968cbb3b5a9bd04c86c47
Selahattin Ünlü <selahattin.unlu@yandex.com>
2019-06-29 18:50:57 +0300

    set configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Selahattin's Blog`,
+    author: `Selahattin Ünlü`,
+    description: `Full-Stack Developer`,
+    siteUrl: `https://blog.selahattinunlu.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `selahattinunlu`,
     },
   },
   plugins: [
